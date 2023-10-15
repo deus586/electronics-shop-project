a1175ad07f8a15165fba89dd1132c36da8562a8b
mikhail <mikhail5861@gmail.com>
2023-10-15 14:17:25 +0300

    hw2

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+from csv import DictReader
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,11 +16,9 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
-
-        self.all.append(self)
 
     def calculate_total_price(self) -> float:
         """
@@ -33,4 +34,25 @@ class Item:
         """
         self.price *= self.pay_rate
 
+    @property
+    def name(self):
+        return self.__name
 
+    @name.setter
+    def name(self, new_name):
+        if len(new_name) < 10:
+            self.__name = new_name
+        else:
+            self.__name = new_name[:10]
+
+    @classmethod
+    def instantiate_from_csv(cls, path):
+        with open(path, 'r', newline='', encoding='Windows-1251') as csv_file:
+            reader = DictReader(csv_file)
+            for row in reader:
+                cls.all.append(cls(row['name'], float(row['price']), int(row['quantity'])))
+        return cls.all
+
+    @staticmethod
+    def string_to_number(number: str):
+        return int(float(number))
