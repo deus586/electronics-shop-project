6f87e5c3d84a856d3e5c0b882fb43c535a483ccc
mikhail <mikhail5861@gmail.com>
2023-10-18 16:20:38 +0300

    hw3

--- src/item.py
+++ src/item.py
@@ -20,6 +20,12 @@ class Item:
         self.price = price
         self.quantity = quantity
 
+    def __str__(self):
+        return self.__name
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.__name}', {self.price}, {self.quantity})"
+
     def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
