e2e7058561cc1ec0030b2aa9ec26a53219d11652
mikhail <mic131053@gmail.com>
2023-10-26 12:11:32 +0300

    add error

--- src/phone.py
+++ src/phone.py
@@ -5,7 +5,18 @@ class Phone(Item):
 
     def __init__(self, name: str, price: float, quantity: int, supported_sim: int) -> None:
         super().__init__(name, price, quantity)
-        self.number_of_sim = supported_sim
+        self.__number_of_sim = supported_sim
 
     def __repr__(self):
         return super().__repr__()[:-1] + f', {self.number_of_sim})'
+
+    @property
+    def number_of_sim(self):
+        return self.__number_of_sim
+
+    @number_of_sim.setter
+    def number_of_sim(self, new_num):
+        if new_num > 0:
+            self.__number_of_sim = new_num
+        else:
+            raise ValueError
